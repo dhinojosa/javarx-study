b02ad916372041f4431a9da88520ac52cdd2c971
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-03-19 08:59:10 -0400

    Added Backpressure Test

---
+++ src/test/java/com/evolutionnext/javarx/ObservableBackpressureTest.java
@@ -0,0 +1,99 @@
+package com.evolutionnext.javarx;
+
+import org.junit.Before;
+import org.junit.Test;
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class ObservableBackpressureTest {
+
+    private Observable<Integer> crazedObservable;
+
+    @Before
+    public void startUp() {
+        crazedObservable = Observable.create(s -> {
+            int i = 0;
+            //noinspection InfiniteLoopStatement
+            while (true) {
+                s.onNext(i);
+                i++;
+            }
+        });
+    }
+
+    @Test
+    public void testBackPressure() throws InterruptedException {
+        crazedObservable.observeOn(Schedulers.newThread()).subscribe(n -> {
+            try {
+                Thread.sleep(5); //Wait to fill the buffer some more.
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(n);
+        }, Throwable::printStackTrace);
+
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testBackPressureWithSample() throws InterruptedException {
+        crazedObservable.sample(250, TimeUnit.MILLISECONDS)
+                .observeOn(Schedulers.newThread()).subscribe(n -> {
+            try {
+                Thread.sleep(5); //Wait to fill the buffer some more.
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(n);
+        }, Throwable::printStackTrace);
+
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testBackPressureWithOnBackPressureBuffer() throws InterruptedException {
+        crazedObservable.onBackpressureBuffer()
+                .observeOn(Schedulers.newThread()).subscribe(n -> {
+            try {
+                Thread.sleep(5); //Wait to fill the buffer some more.
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(n);
+        }, Throwable::printStackTrace);
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testBackPressureWithOnBackpressureDrop() throws InterruptedException {
+        crazedObservable.onBackpressureDrop()
+                .observeOn(Schedulers.newThread()).subscribe(n -> {
+            try {
+                Thread.sleep(5); //Wait to fill the buffer some more.
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(n);
+        }, Throwable::printStackTrace);
+
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testBackPressureWithOnBackpressureLatest() throws InterruptedException {
+        crazedObservable.onBackpressureLatest()
+                .observeOn(Schedulers.newThread()).subscribe(n -> {
+            try {
+                Thread.sleep(5); //Wait to fill the buffer some more.
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(n);
+        }, Throwable::printStackTrace);
+
+        Thread.sleep(10000);
+    }
+}
